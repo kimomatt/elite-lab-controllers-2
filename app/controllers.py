861463d7541bb08f7ab0de2fca5b1984b213805a
Daddy Kimotsuki <Kimo@shoichi-kimotsukis-imac-239.local>
2021-03-03 20:20:58 -0800

    <chatstuff>

--- app/controllers.py
+++ app/controllers.py
@@ -32,3 +32,26 @@ def create_session():
 def get_username_from_token(token):
     username = SessionManager.get_username(token)
     return {"username": username}
+@app.route('/chat/<string:chat_id>/last', methods=['GET'])
+def get_last_messages_in_chat(chat_id):
+    count = request.args.get('count', default=100)
+    result = MessageManager.get_last_messages_in_chat(chat_id, count)
+    response = []
+    for message in result:
+            response.append(message.to_dict())
+    return{"messages": response}
+
+@app.route('/messages/', methods=['POST'])
+def create_message():
+    body = request.json
+    message = MessageManager.create_message(body)
+    return {"id": message.id}
+
+@app.route('/chat/<string:chat_id>/updates', methods=['GET'])
+def get_chat_updates(chat_id):
+    last_id = request.args.get('ref_id', default=0)
+    response = []
+    result = MessageManager.get_message_updates_in_chat(chat_id, last_id)
+    for message in result:
+            response.append(message.to_dict())
+    return{"messages": response}
